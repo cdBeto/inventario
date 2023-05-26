257911ee6ccb42bd232e0c66bb0dd702d55fccb9
FranciscoPalaciosF <francisco.palacios@uabc.edu.mx>
2023-05-26 16:02:50 -0700

    Modificación de inicio.html

--- inventario/settings.py
+++ inventario/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'MalvadosYAsosiad$articulos',
-        'USER': 'MalvadosYAsosiad',
-        'PASSWORD': 'a9474452a',
-        'HOST': 'MalvadosYAsosiados.mysql.pythonanywhere-services.com',
+        'NAME': 'articulos',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
