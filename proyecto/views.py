51c08fcbb13224b9d4576500693afe4642f86dbb
FranciscoPalaciosF <francisco.palacios@uabc.edu.mx>
2023-05-26 13:52:29 -0700

    Commit de prueba

---
+++ proyecto/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Articulo
+from .forms import ArticuloForm
+# Create your views here.
+
+#acceso a las paginas
+def inicio(request): 
+    return render(request, 'paginas/inicio.html')
+
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+
+#acceso a los articulos
+def articulos(request):
+    articulos = Articulo.objects.all()
+    return render(request, 'articulos/index.html', {'articulos': articulos})
+
+def crear(request):
+    formulario = ArticuloForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('articulos')
+    return render(request, 'articulos/crear.html', {'formulario': formulario})
+
+def editar(request,id):
+    articulo = Articulo.objects.get(id=id)
+    formulario = ArticuloForm(request.POST or None, request.FILES or None, instance=articulo)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('articulos')
+    return render(request, 'articulos/editar.html', {'formulario': formulario})
+
+def eliminar(request, id):
+    articulo = Articulo.objects.get(id=id)
+    articulo.delete()
+    return redirect('articulos')
