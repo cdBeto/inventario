97c392bc303acea4afc68fe74e421a0c85dd7864
FranciscoPalaciosF <francisco.palacios@uabc.edu.mx>
2023-05-26 14:00:12 -0700

    Creación de databases.txt para alternar entre local y pythonanywhere

--- inventario/settings.py
+++ inventario/settings.py
@@ -79,11 +79,12 @@ DATABASES = {
     'default': {
         
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'articulos',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'NAME': 'MalvadosYAsosiad$articulos',
+        'USER': 'MalvadosYAsosiad',
+        'PASSWORD': 'a9474452a',
+        'HOST': 'MalvadosYAsosiados.mysql.pythonanywhere-services.com',
         'PORT': '3306',
+
 
     }
 }
