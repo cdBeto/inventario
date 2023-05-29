5047cbd16405e2f83e35aab5d4cd5a79608fc3cc
FranciscoPalaciosF <francisco.palacios@uabc.edu.mx>
2023-05-29 09:20:17 -0700

    Otra correcion

--- proyecto/models.py
+++ proyecto/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Articulo(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, verbose_name='Nombre')
     cantidad = models.CharField(max_length=10, verbose_name='Cantidad')
-    descripcion = models.TextField(verbose_name='Descripcion', null=True )
+    descripcion = models.TextField(verbose_name='Descripcion', null=True)
 
     def __str__(self):
         fila = "Nombre: " + self.nombre + " - " + "Cantidad: " + self.cantidad + " - " + "Descripcion: " + self.descripcion
